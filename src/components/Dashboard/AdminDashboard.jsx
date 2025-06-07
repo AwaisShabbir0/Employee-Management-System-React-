b1f98f5f17bbceeee1be69c6564eea5076b20ccb
Awaiskamboh0810 <awaiskamboh0810@gmail.com>
2025-06-07 15:05:45 +0500

    Admin Dashboard implementation

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -5,6 +5,14 @@ const AdminDashboard = () => {
   return (
     <div className='h-screen w-screen p-10'>
         <Header/>
+        <div>
+          <form action="">
+            <h3>Task Title</h3>
+            <input type="text" placeholder='Write Something you want to do...' />
+            <h1>Description</h1>
+            <textarea name="" id="" cols='30' rows='10'></textarea>
+          </form>
+        </div>
     </div>
   )
 }
