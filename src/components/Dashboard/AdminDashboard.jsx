3534c9f9633fb3c5cb833c1c5753afa7a5147c96
Awaiskamboh0810 <awaiskamboh0810@gmail.com>
2025-07-21 23:26:28 +0500

    Enhancing Admin Dashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import Header from '../others/Header'
+import React from "react";
+import Header from "../others/Header";
 
 const AdminDashboard = () => {
   return (
-    <div className='h-screen w-screen p-10'>
-        <Header/>
-        <div>
-          <form action="">
-            <h3>Task Title</h3>
-            <input type="text" placeholder='Write Something you want to do...' />
-            <h1>Description</h1>
-            <textarea name="" id="" cols='30' rows='10'></textarea>
-          </form>
-        </div>
-    </div>
-  )
-}
+    <div className="h-screen w-screen p-10">
+      <Header />
+      <div>
+        <form className="flex  flex-wrap w-full  bg-red-200 items-start justify-between" action="">
+          <div className="w-1/2">
+            <div>
+              <h3>Task Title</h3>
+              <input
+                type="text"
+                placeholder="what you want..."
+              />
+            </div>
+             <div>
+            <h3>Date</h3>
+            <input type="date" />
+          </div>
+          <div>
+            <h3>Assign to</h3>
+            <input type="text" placeholder="employee name" />
+          </div>
+          <div>
+            <h3>cataegory</h3>
+            <input type="text" placeholder="design, dev, etc" />
+          </div>
+          </div>
 
-export default AdminDashboard
+          <div className="w-1/2">
+            <h1>Description</h1>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+         
+          <button>Create task</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
